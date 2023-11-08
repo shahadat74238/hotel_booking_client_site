6953481aa6eac3a84cf543328aee6781dec80ade
Shahadat Hosen <shahadat74238@gmail.com>
2023-11-08 22:40:13 +0600

    modify footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import Subscription from "../../Pages/Home/Subscription";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded mt-20">
+      <footer className="footer footer-center p-10 bg-gray-700 text-base-content rounded mt-20">
         <nav>
           <Subscription />
         </nav>
@@ -45,7 +45,7 @@ const Footer = () => {
           </div>
         </nav>
         <aside>
-          <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+          <p>Copyright © 2023 - All right reserved by Five Star</p>
         </aside>
       </footer>
     </div>
