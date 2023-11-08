e65120b40ffb2ade346b9b3ddb32279ab8baa400
Shahadat Hosen <shahadat74238@gmail.com>
2023-11-08 23:15:08 +0600

    modify loading components

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ const Loading = () => {
     <div className="h-screen flex justify-center items-center">
       <div>
         <h1 className="font-semibold text-center text-2xl ">Loading Data...</h1>
-        <p>
+        <p className="text-center">
           <span className="loading loading-bars loading-lg"></span>
         </p>
       </div>
