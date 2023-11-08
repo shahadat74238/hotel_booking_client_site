2aec010ab565b84c6e64380e695806abb912daea
Shahadat Hosen <shahadat74238@gmail.com>
2023-11-08 22:46:07 +0600

    create loading components

---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+const Loading = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div>
+        <h1 className="font-semibold text-center text-2xl ">Loading Data...</h1>
+        <p>
+          <span className="loading loading-bars loading-lg"></span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
